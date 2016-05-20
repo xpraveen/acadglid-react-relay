a236a3b3c48959b05f0d817de7a51fe2b870a670
Praveen Singh <praveenkumar_singh@intuit.com>
2016-05-20 19:19:26 +0530

    session10

--- data/schema.js
+++ data/schema.js
@@ -1,10 +1,15 @@
 import {GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt} from "graphql";
 
+import {
+    globalIdField
+} from "graphql-relay";
+
 let helloCounter = 0;
 
-const query = new GraphQLObjectType({
-    name: "Query",
+const bookStoreType = new GraphQLObjectType({
+    name: "BookStore",
     fields: () => ({
+        id: globalIdField("BookStore"),
         hello: {
             type: GraphQLString,
             resolve: () => {
@@ -15,6 +20,21 @@ const query = new GraphQLObjectType({
         counter: {
             type: GraphQLInt,
             resolve: () => helloCounter
+        }
+    })
+});
+
+
+class BookStore {}
+let bookStore = new BookStore();
+
+
+const query = new GraphQLObjectType({
+    name: "Query",
+    fields: () => ({
+        bookStore: {
+            type: bookStoreType,
+            resolve: () => (bookStore)
         }
     })
 });
