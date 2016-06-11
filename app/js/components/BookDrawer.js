3b07ffbd870e94e4e2716fc98c8dc1eada05aac6
Praveen Singh <praveenkumar_singh@intuit.com>
2016-06-12 01:14:42 +0530

    d

--- app/js/components/BookDrawer.js
+++ app/js/components/BookDrawer.js
@@ -4,7 +4,7 @@ export default class BookDrawer extends React.Component {
 
     constructor(){
         super();
-        this.state = {};
+        this.state = {"title": ""};
     }
     closeBookDrawer = () => {
         this.props.closeBookDrawer();
