3af787d5ed02fd24329141ca74d7a5870ab8d6ba
Praveen Singh <praveenkumar_singh@intuit.com>
2016-06-12 02:37:47 +0530

    d

--- app/js/components/BookDrawer.js
+++ app/js/components/BookDrawer.js
@@ -7,7 +7,9 @@ export default class BookDrawer extends React.Component {
         this.state = {
             "title": "",
             "isbn": "",
-            "titleValid": true
+            "titleValid": true,
+            "isbnValid": true,
+            "formValid": false
         };
     }
     closeBookDrawer = () => {
@@ -22,26 +24,41 @@ export default class BookDrawer extends React.Component {
         this.props.closeBookDrawer();
     }
 
+    handleChange = (event) =>{
+        const {name, value} = event.target;
+        this.setState({
+            [`${name}`]: value
+        });
+    }
+
+    handleValidity = (event) =>{
+        const {name, validity} = event.target;
+        this.setState({
+            [`${name}Valid`]: validity.valid,
+            "formValid": validity.valid
+        });
+    }
+
     render() {
-        const {title, isbn, titleValid} = this.state;
+        const {title, isbn, titleValid, isbnValid, formValid} = this.state;
         return (
             <div className="book-drawer">
                 <h1>Book</h1>
                 <form className="add-book form" onSubmit={this.addBook}>
                     <div className="form-group">
-                        <input type="text" value={title} className={titleValid
+                        <input type="text" name="title" value={title} className={titleValid
                             ? "form-control"
-                            : "form-control invalid"} onBlur={e => this.setState({"titleValid": e.target.validity.valid})} onChange={e => this.setState({"title": e.target.value})} placeholder="Add a Book Title" required={true} pattern="[A-Za-z-0-9\s]+"/>
+                            : "form-control invalid"} onBlur={this.handleValidity} onChange={this.handleChange} placeholder="Add a Book Title" required={true} pattern="[A-Za-z-0-9\s]+"/>
                     </div>
                     <div className="form-group">
                         <label>ISBN</label>
-                        <input type="text" value={isbn} className={titleValid
+                        <input type="text" name="isbn" value={isbn} className={isbnValid
                             ? "form-control"
-                            : "form-control invalid"} onBlur={e => this.setState({"titleValid": e.target.validity.valid})} onChange={e => this.setState({"isbn": e.target.value})} placeholder="Add a ISBN" required={true} pattern="[0-9]+"/>
+                            : "form-control invalid"} onBlur={this.handleValidity} onChange={this.handleChange} placeholder="Add a ISBN" required={true} pattern="[0-9]+"/>
                     </div>
                     <div className="form-group">
                         <button type="button" className="btn" onClick={this.closeBookDrawer}>Cancel</button>
-                        <button type="submit" className="btn btn-primary float-right">Add Book</button>
+                        <button type="submit" disabled={!formValid} className="btn btn-primary float-right">Add Book</button>
                     </div>
                 </form>
             </div>
