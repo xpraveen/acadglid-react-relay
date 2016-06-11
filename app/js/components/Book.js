8eabcfe6b3f963fe4cbeb181c2b9c2801d6a89a9
Praveen Singh <praveenkumar_singh@intuit.com>
2016-06-11 22:22:38 +0530

    d

--- app/js/components/Book.js
+++ app/js/components/Book.js
@@ -2,20 +2,31 @@ import React from "react";
 
 export default class Book extends React.Component {
 
+    constructor(){
+        super();
+        this.state = {"deleting": false};
+    }
     handleBookDelete = (event) => {
         const {book} = this.props;
         event.stopPropagation();
-        this.props.deleteBook(book.id);
+        this.setState({"deleting": true});
+
+        setTimeout(() => {
+            this.props.deleteBook(book.id);
+            this.setState({"deleting": false});
+        },3000);
+
     }
     render() {
         const {book, active} = this.props;
+        const {deleting} = this.state;
 
         return (
             <tr data-book-id={book.id} onClick={this.props.onClick}>
                 <td>{book.id}</td>
                 <td>{book.title}</td>
                 <td>
-                    <button type="button" className={active? "btn btn-danger" : "hide"} onClick={this.handleBookDelete}>Delete</button>
+                    <button type="button" disabled={deleting} className={active? "btn btn-danger" : "hide"} onClick={this.handleBookDelete}>{deleting ? "Deleting...": "Delete"}</button>
                 </td>
             </tr>
         );
