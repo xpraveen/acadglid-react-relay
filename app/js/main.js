9f243c31d53b3e18ba3f6379d34828e7d5a34a08
Praveen Singh <praveenkumar_singh@intuit.com>
2016-06-13 20:10:07 +0530

    d

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,9 @@ import BookStoreRoute from "./routes/BookStoreRoute";
 
 import "!style!css!sass!../sass/app.scss";
 
-ReactDOM.render(<Relay.RootContainer Component={App} route={new BookStoreRoute()}/>, document.getElementById("app"));
+ReactDOM.render(
+    <Relay.RootContainer renderLoading={() => {
+    return (
+        <div>Loading...</div>
+    );
+}} Component={App} route={new BookStoreRoute()}/>, document.getElementById("app"));
