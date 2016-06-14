1c84f94a8f881776b484fd7ad7fa2ec3714c95ca
Praveen Singh <praveenkumar_singh@intuit.com>
2016-06-14 21:15:21 +0530

    e

--- app/js/main.js
+++ app/js/main.js
@@ -8,7 +8,17 @@ import "!style!css!sass!../sass/app.scss";
 
 ReactDOM.render(
     <Relay.RootContainer renderLoading={() => {
-    return (
-        <div>Loading...</div>
-    );
-}} Component={App} route={new BookStoreRoute()}/>, document.getElementById("app"));
+        return (
+            <div>Loading...</div>
+        );
+    }}
+
+        renderFailure={(error) => {
+            const style = {
+                color: "red"
+            };
+            return (
+                <label style={style}>{error.message}</label>
+            );
+        }}
+        Component={App} route={new BookStoreRoute()}/>, document.getElementById("app"));
