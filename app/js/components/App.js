5cbd1dca917f39b9c61f79ba9bfe706cb201d1f1
Praveen Singh <praveenkumar_singh@intuit.com>
2016-06-16 01:49:57 +0530

    d

--- app/js/components/App.js
+++ app/js/components/App.js
@@ -5,8 +5,32 @@ export default class App extends React.Component {
     render() {
         return (
             <div className="app container">
-                <div className="hello-text bold red">Hello World!</div>
-                <h1>Hello World with Twitter Bootstrap!</h1>
+                <br/>
+                <div className="alert alert-success" role="alert">A sample success message</div>
+                <br/>
+                <div className="alert alert-info" role="alert">A sample Alert message</div>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>John</td>
+                            <td>Dae</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Mark</td>
+                            <td>Doe</td>
+                        </tr>
+                    </tbody>
+                </table>
+
             </div>
         );
     }
