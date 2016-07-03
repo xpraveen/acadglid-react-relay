845bb05dd056969d8a760f670e1b1898aecbb588
Praveen Singh <praveenkumar_singh@intuit.com>
2016-07-04 03:42:43 +0530

    d

--- app/js/components/Lifecycle.js
+++ app/js/components/Lifecycle.js
@@ -4,36 +4,12 @@ export default class App extends React.Component {
 
     constructor(props) {
         super();
-        console.log("constructor: props: ", props);
-    }
-
-    componentWillMount() {
-        console.log("componentWillMount");
-    }
-
-    componentDidMount() {
-        console.log("componentDidMount");
     }
 
     componentWillReceiveProps(nextProps) {
+        console.log("componentWillReceiveProps: prevProps: ", this.props);
         console.log("componentWillReceiveProps: nextProps: ", nextProps);
-    }
-
-    shouldComponentUpdate() {
-        console.log("shouldComponentUpdate");
-        return false;
-    }
-
-    componentWillUpdate() {
-        console.log("componentWillUpdate");
-    }
-
-    componentDidUpdate() {
-        console.log("componentDidUpdate");
-    }
-
-    componentWillUnmount() {
-        console.log("componentWillUnmount");
+        console.log(`Moving from ${this.props.hello} to ${nextProps.hello}`);
     }
 
     render() {
